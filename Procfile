eabfa1412d85f13967a552d197a6203c3e425c5c
DIPJYOTI SAIKIA <dipjyotisaikia46@gmail.com>
2025-09-16 08:42:05 +0530

    first commit

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -c gunicorn_config.py
+web: gunicorn backend.wsgi:application -c gunicorn_config.py
--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,6 +1,7 @@
 bind = "0.0.0.0:8000"
 workers = 2
-wsgi_application = "backend.wsgi:application"
 pythonpath = "."
 accesslog = "-"
 errorlog = "-"
+wsgi_app = "backend.wsgi:application"
+chdir = "/opt/render/project/src/"
